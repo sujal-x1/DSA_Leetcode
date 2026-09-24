b5a0309de0597065e9b5ae7603bf3c7b3fa27d62
sujal-x1 <sujalpatil1532@gmail.com>
2026-09-24 20:55:35 +0530

    Delete solutions directory

--- solutions/176-SecondHighestSalary/176-SecondHighestSalary.sql
+++
@@ -1,3 +0,0 @@
--- Last updated: 9/24/2026, 8:52:26 PM
-# Write your MySQL query statement below
-select max(e.salary)as secondHighestSalary from employee e where e.salary<(select max(salary)from employee e1);
