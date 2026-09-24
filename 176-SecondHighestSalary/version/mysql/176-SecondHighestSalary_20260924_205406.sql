61215223b616bad3e02aa4194df304afd3cc8875
sujal-x1 <sujalpatil1532@gmail.com>
2026-09-24 23:54:50 +0530

    Delete 176-SecondHighestSalary/version/mysql directory

--- 176-SecondHighestSalary/version/mysql/176-SecondHighestSalary_20260924_205406.sql
+++
@@ -1,3 +0,0 @@
--- Last updated: 9/24/2026, 8:54:06 PM
-# Write your MySQL query statement below
-select max(e.salary)as secondHighestSalary from employee e where e.salary<(select max(salary)from employee e1);
